64d9865e68d122828bac909a93f9e906bf112f8a
Badichan Dmitriy <badimalex@gmail.com>
2018-08-05 22:09:25 +0200

    CoursesPage

--- src/components/App.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export interface AppProps {}
-
-export function App(props: AppProps) {
-  return <div>Hello App</div>;
-}
---
+++ src/constants/courses.ts
@@ -0,0 +1,65 @@
+export default [
+  {
+    picture: 'http://placehold.it/32x32',
+    title: 'Course number 0',
+    description:
+      'Ea occaecat tempor ut non cupidatat sit aute occaecat id minim ea cillum. Adipisicing excepteur dolore aliquip nostrud labore irure deserunt incididunt id ullamco. Enim dolor occaecat cillum enim labore anim magna est dolor. Nulla nulla occaecat ipsum minim qui est ipsum in eiusmod reprehenderit exercitation cillum amet nostrud. Officia commodo in ad voluptate magna irure.\r\n',
+    rating: 1,
+    type: 'бесплатный',
+    reviews: 98
+  },
+  {
+    picture: 'http://placehold.it/32x32',
+    title: 'Course number 1',
+    description:
+      'Magna reprehenderit nostrud non est. Est occaecat anim labore proident nisi do. Ea adipisicing quis aliquip eu ad consectetur adipisicing anim pariatur deserunt ut aliqua nostrud esse. Officia cupidatat deserunt deserunt incididunt.\r\n',
+    rating: 5,
+    type: 'платный',
+    reviews: 76
+  },
+  {
+    picture: 'http://placehold.it/32x32',
+    title: 'Course number 2',
+    description:
+      'Adipisicing pariatur ea ullamco incididunt. Sint id voluptate reprehenderit fugiat nulla do consequat culpa commodo. Enim sunt laboris pariatur occaecat quis quis non. Do cillum ex nulla consequat quis in irure id voluptate ipsum Lorem voluptate eu. Irure adipisicing qui aliquip adipisicing fugiat in sit enim sint amet duis deserunt. Sit anim aute consequat magna cupidatat tempor fugiat reprehenderit sit sint. Est dolore enim amet elit Lorem mollit eu pariatur Lorem veniam voluptate magna officia.\r\n',
+    rating: 5,
+    type: 'бесплатный',
+    reviews: 77
+  },
+  {
+    picture: 'http://placehold.it/32x32',
+    title: 'Course number 3',
+    description:
+      'Occaecat proident nisi velit anim laborum labore elit esse nisi culpa labore do aliquip culpa. Veniam proident officia quis occaecat eu qui aliqua dolor aliqua. Incididunt sit et ex enim in. Duis eiusmod ipsum sint esse culpa exercitation nulla anim.\r\n',
+    rating: 3,
+    type: 'бесплатный',
+    reviews: 74
+  },
+  {
+    picture: 'http://placehold.it/32x32',
+    title: 'Course number 4',
+    description:
+      'Dolore anim proident irure incididunt labore esse et anim dolor Lorem quis eu veniam. Exercitation Lorem mollit officia minim deserunt voluptate eu. Aute anim incididunt id ex ad culpa. Mollit anim cupidatat do esse quis veniam excepteur do sint duis ex.\r\n',
+    rating: 1,
+    type: 'бесплатный',
+    reviews: 138
+  },
+  {
+    picture: 'http://placehold.it/32x32',
+    title: 'Course number 5',
+    description:
+      'Consectetur officia sit sit ea commodo eu magna Lorem qui elit mollit aliqua nostrud. Reprehenderit cupidatat velit ut cupidatat velit id cupidatat ipsum. Veniam do nisi cupidatat amet aliquip irure eiusmod occaecat sint dolore. Nostrud tempor quis elit labore aliqua voluptate eiusmod magna irure. Dolor eiusmod non quis magna ea. Nostrud magna excepteur officia est deserunt excepteur voluptate. Mollit dolore eu consequat irure id veniam irure amet proident in.\r\n',
+    rating: 4,
+    type: 'платный',
+    reviews: 132
+  },
+  {
+    picture: 'http://placehold.it/32x32',
+    title: 'Course number 6',
+    description:
+      'Adipisicing qui duis duis laboris irure in et id pariatur. Fugiat incididunt consectetur qui irure quis pariatur ea consectetur. Commodo nostrud adipisicing nostrud adipisicing commodo eu deserunt enim amet enim proident sunt id. Non eiusmod laboris occaecat veniam in reprehenderit est nisi do deserunt nisi. Lorem reprehenderit dolore dolore dolore commodo aliquip Lorem sunt aliqua excepteur velit enim. Magna voluptate nulla mollit sunt laborum esse. Veniam incididunt dolore do velit minim nisi qui.\r\n',
+    rating: 5,
+    type: 'бесплатный',
+    reviews: 109
+  }
+];
